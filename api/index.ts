78f9f352b61823f662a2ca0f6eb165576c152849
ivgtr <gear.1112t.o@gmail.com>
2021-04-07 19:50:14 +0900

    :sparkles: change: cacheを設定しとく

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,16 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import type { query } from '../src/@types/Query.d'
 import { createElement } from '../src/svg'
 
+const CACHE_MAX_AGE = 60 * 60 * 2
+
 export default async (req: VercelRequest & { query: query }, res: VercelResponse) => {
   const { text, url, width, height, color, darkColor, fontSize } = req.query
 
   const svg = await createElement({ text, url, width, height, color, darkColor, fontSize })
 
   res.writeHead(200, {
-    'Content-Type': 'image/svg+xml'
+    'Content-Type': 'image/svg+xml',
+    'Cache-Control': `public, max-age=${CACHE_MAX_AGE}`
   })
   res.end(svg)
 }
