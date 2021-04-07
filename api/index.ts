478cb3c598bdc7b46df2242256dd1ecc7c09a351
ivgtr <gear.1112t.o@gmail.com>
2021-04-07 19:21:17 +0900

    :ambulance: fix: typeはimportする

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { query } from '../src/@types/Query.d'
 import { createElement } from '../src/svg'
-
-type query = {
-  text?: string
-  url?: string
-  width?: string
-  height?: string
-  color?: string
-  darkColor?: string
-  fontSize?: string
-}
 
 export default async (req: VercelRequest & { query: query }, res: VercelResponse) => {
   const { text, url, width, height, color, darkColor, fontSize } = req.query
